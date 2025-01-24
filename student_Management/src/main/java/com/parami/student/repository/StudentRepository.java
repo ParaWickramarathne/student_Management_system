2010c277787acef02f4f35b7cd3c41b357850b01
Parami Wickramarathna <121754884+ParaWickramarathne@users.noreply.github.com>
2025-01-24 18:53:13 +0530

    Update StudentRepository.java
    com del

--- student_Management/src/main/java/com/parami/student/repository/StudentRepository.java
+++ student_Management/src/main/java/com/parami/student/repository/StudentRepository.java
@@ -7,12 +7,5 @@ import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    // No need to write any implementation code, Spring Data JPA handles it for us.
-
-    // Add a method to sort students by their first name or any other field
-
-    // public List<Student> findAllByOrderByFirstNameAsc();
-
-    // You can sort by first name in ascending order
-
+    
 }
